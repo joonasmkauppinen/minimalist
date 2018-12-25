778956d4c429f9f0dadfff3484177de84c509a4e
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2018-12-25 20:40:17 +0200

    Many updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,77 @@
 'use stict'
 
+const todoItems = []
+
 const todoList = document.getElementById('list')
 const input = document.getElementById('new-list-item')
 
 const addNewTodo = (value) => {
-	const li  = document.createElement('li')
-	const img = document.createElement('img')
-	const p   = document.createElement('p')
+	const li              = document.createElement('li')
+	const checkmark       = document.createElement('span')
+	const checkmarkRing   = document.createElement('img')
+	const checkmarkCheck  = document.createElement('img')
+	const p               = document.createElement('p')
 
-	img.setAttribute('src', './src/svg/circle_ic.svg')
-	img.classList.add('clickable')
-	li.appendChild(img)
+	checkmarkRing.setAttribute('src', './src/svg/circle_ic.svg')
+	checkmarkRing.classList.add('card__checkmark-ring')
+
+	checkmarkCheck.setAttribute('src', './src/svg/checkmark_ic.svg')
+	checkmarkCheck.classList.add('card__checkmark-check' ,'card__checkmark-check--hidden')
+
+	checkmark.appendChild(checkmarkRing)
+	checkmark.appendChild(checkmarkCheck)
+	checkmark.classList.add('card__checkmark', 'clickable')
+
+	li.appendChild(checkmark)
 
 	p.innerText = value
 	p.classList.add('clickable')
-	p.classList.add('list-p')
-	p.addEventListener('click', e => {
-		e.target.classList.toggle('list-p-checked')
-		console.log('Clicked on item: ', e.target.innerText)
-	})
+	p.classList.add('card__p')
 	li.appendChild(p)
 
 	li.classList.add('card')
 
+	const checkTarget = (event) => {
+		return new Promise(resolve => {
+			const tag = event.target.tagName
+			if (tag === "P" || tag === "IMG") {
+				resolve(true)
+			} else {
+				resolve(false)
+			}
+		})
+	}
+
+	li.addEventListener('mouseover', e => {
+		// Add hover style
+		checkTarget(e).then( res => {
+			if (res) p.classList.add('card__p--hover')
+		})
+	})
+
+	li.addEventListener('mouseout', e => {
+		// Remove hover style
+		checkTarget(e).then( res => {
+			if (res) p.classList.remove('card__p--hover')
+		})
+	})
+
+	li.addEventListener('click', e => {
+		checkTarget(e).then(res => {
+			if (res) {
+				// Toggle todo checked
+				checkmarkRing.classList.toggle('card__checkmark-ring--hidden')
+				checkmarkCheck.classList.toggle('card__checkmark-check--hidden')
+				p.classList.toggle('card__p--checked')
+				const index = Array.prototype.indexOf.call(todoList.childNodes, li) - 1
+				todoItems[index].checked = (todoItems[index].checked ? false : true)
+			}
+		})
+	})
+
+	todoItems.unshift({checked: false, value: value})
+
+	// Append new item on top of list
 	todoList.insertBefore(li, todoList.children[0])
 }
 
