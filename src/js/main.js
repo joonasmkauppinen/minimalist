25b6ca460626661fa236a81bd56142fb563544ec
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2018-12-25 23:08:17 +0200

    Update: todo item actions

--- src/js/main.js
+++ src/js/main.js
@@ -33,8 +33,34 @@ const addNewTodo = (value) => {
 
 	cardActionsArrow.setAttribute('src', './src/svg/item_actions_ic.svg')
 	cardActionsArrow.classList.add('card__actions-arrow')
+
 	cardActionsAction.setAttribute('src', './src/img/edit_ic.png')
 	cardActionsAction.classList.add('clickable')
+	cardActionsAction.addEventListener('click', e => {
+
+		const parent = e.target.closest('li')
+		const todoValue = parent.children[1]
+		const index = Array.prototype.indexOf.call(todoList.childNodes, parent) - 1
+
+		if (todoItems[index].checked) {
+			li.removeEventListener('click', () => {})
+			todoList.removeChild(li);
+			todoItems.splice(index, 1)
+			return
+		}
+
+		if (todoItems[index].editable) {
+			cardActionsAction.setAttribute('src', './src/img/edit_ic.png')
+			todoItems[index].editable = false
+			todoValue.contentEditable = false
+		} else {
+			cardActionsAction.setAttribute('src', './src/img/save_ic.png')
+			todoItems[index].editable = true
+			todoValue.contentEditable = true
+			todoValue.focus()
+		}
+	})
+
 	cardActions.classList.add('card__actions')
 	cardActions.appendChild(cardActionsArrow)
 	cardActions.appendChild(cardActionsAction)
@@ -42,7 +68,7 @@ const addNewTodo = (value) => {
 
 	li.classList.add('card')
 
-	const checkTarget = (event) => {
+	const checkTarget = event => {
 		return new Promise(resolve => {
 			const isCheckable = event.target.classList.contains('checkable')
 			if (isCheckable) {
@@ -54,6 +80,8 @@ const addNewTodo = (value) => {
 	}
 
 	li.addEventListener('mouseover', e => {
+		const index = Array.prototype.indexOf.call(todoList.childNodes, li) - 1
+		if (todoItems[index].editable) return
 		// Add hover style
 		checkTarget(e).then( res => {
 			if (res) p.classList.add('card__p--hover')
@@ -68,19 +96,27 @@ const addNewTodo = (value) => {
 	})
 
 	li.addEventListener('click', e => {
-		checkTarget(e).then(res => {
-			if (res) {
+		const index = Array.prototype.indexOf.call(todoList.childNodes, li) - 1
+		if (todoItems[index].editable) return
+		checkTarget(e).then(success => {
+			if (success) {
 				// Toggle todo checked
 				checkmarkRing.classList.toggle('card__checkmark-ring--hidden')
 				checkmarkCheck.classList.toggle('card__checkmark-check--hidden')
 				p.classList.toggle('card__p--checked')
 				const index = Array.prototype.indexOf.call(todoList.childNodes, li) - 1
-				todoItems[index].checked = (todoItems[index].checked ? false : true)
+				if (!todoItems[index].checked) {
+					todoItems[index].checked = true
+					cardActionsAction.setAttribute('src', './src/img/delete_ic.png')
+				} else {
+					todoItems[index].checked = false
+					cardActionsAction.setAttribute('src', './src/img/edit_ic.png')
+				}
 			}
 		})
 	})
 
-	todoItems.unshift({checked: false, value: value})
+	todoItems.unshift({checked: false, editable: false, value: value})
 
 	// Append new item on top of list
 	todoList.insertBefore(li, todoList.children[0])
@@ -89,8 +125,10 @@ const addNewTodo = (value) => {
 input.addEventListener('keydown', e => {
 
 	if (e.keyCode === 13 && input.value !== "") {
-		console.log('adding new todo...')
-		addNewTodo(input.value)
+		if (input.value.trim(' ') !== "") {
+			console.log('adding new todo...')
+			addNewTodo(input.value)
+		}
 		input.value = ""
 	}
 
